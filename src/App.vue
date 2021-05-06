30374113fec3e2b3448308bf4cd5a3951826451a
zhangrenjian <509634685@qq.com>
2021-05-06 15:36:47 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <div class="a">
-      <div class="b">
-        b容器里面的呢人
-      </div>
-         <div class="c">
-           cl里面的 内容
-           <ul>
-             <li v-for="item in nameArr">
-               {{ item }}
-             </li>
-           </ul>
-         </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <div class="a">
+            <div class="b">
+                b容器里面的呢人
+            </div>
+            <div class="c">
+                cl里面的 内容
+                <ul>
+                    <li v-for="item in nameArr">
+                        {{ item }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
+    name: 'App',
+    components: {
     // HelloWorld
-  },
-  data() {
-    return{
-      name:"121",
-      age: 'age',
-      nameArr: ['1','2','3']
-    }
-  },
-  methods: {
-    handClick() {
-      console.log(111);
     },
-    aa() {
-          let a  
-      const b = 1;
-           const c = 'asdsa'
+    data() {
+        return {
+            name: "121",
+            age: 'age',
+            nameArr: ['1', '2', '3']
+        }
+    },
+    methods: {
+        handClick() {
+            console.log(111);
+        },
+        aa() {
+            let a
+            const b = 1;
+            const c = 'asdsa'
+        }
     }
-  }
 }
 </script>
 
